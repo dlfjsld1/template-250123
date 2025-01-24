fbc1d372dde15cfefa8debaa4e0809f93c40b211
dlfjsld1 <ltk900216@gmail.com>
2025-01-24 11:49:34 +0900

    list html

--- src/main/java/com/example/template/domain/post/post/controller/PostController.java
+++ src/main/java/com/example/template/domain/post/post/controller/PostController.java
@@ -7,11 +7,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,34 +19,36 @@ import java.util.stream.Collectors;
 @RequestMapping("/posts")
 public class PostController {
 
-    private List<Post> posts = new ArrayList<>();
+    private List<Post> posts = new ArrayList();
     private long lastId = 3L;
 
     public PostController() {
         Post p1 = Post.builder()
                 .id(1L)
                 .title("title1")
-                .content("content1")
+                .content("title1")
                 .build();
+
         Post p2 = Post.builder()
                 .id(2L)
                 .title("title2")
-                .content("content2")
+                .content("title2")
                 .build();
+
         Post p3 = Post.builder()
                 .id(3L)
                 .title("title3")
-                .content("content3")
+                .content("title3")
                 .build();
+
         posts.add(p1);
         posts.add(p2);
         posts.add(p3);
     }
 
     @GetMapping("/write")
-    @ResponseBody // View를 사용하지 않고, 데이터를 그대로 응답
-    public String showWrite() {
-        return getFormHtml("", "", "");
+    public String showWrite(WriteForm form, BindingResult bindingResult) {
+        return "domain/post/post/write";
     }
 
     @AllArgsConstructor
@@ -62,58 +62,51 @@ public class PostController {
         private String content;
     }
 
+
+    // @ResponseBody를 빼면 반환값을 템플릿으로 인식한다.
     @PostMapping("/write")
-    public String doWrite(@Valid WriteForm form, BindingResult bindingResult) {
+    public String doWrite(@Valid WriteForm form, BindingResult bindingResult,
+                          Model model) {
 
         if (bindingResult.hasErrors()) {
 
-            String errorMessage = bindingResult.getFieldErrors()
-                    .stream()
-                    .map(err -> err.getDefaultMessage())
-                    .sorted()
-                    .map(msg->msg.split("-")[1])
-                    .collect(Collectors.joining("<br>"));
+//            String errorMessage = bindingResult.getFieldErrors()
+//                    .stream()
+//                    .map(err -> err.getDefaultMessage())
+//                    .sorted()
+//                    .map(msg -> msg.split("-")[1])
+//                    .collect(Collectors.joining("<br>"));
+//
+//            model.addAttribute("errorMessage", errorMessage);
 
-            return getFormHtml(errorMessage, form.getTitle(), form.getContent());
+            return "domain/post/post/write";
         }
 
         Post post = Post.builder()
-                .id(lastId++)
+                .id(++lastId)
                 .title(form.getTitle())
                 .content(form.getContent())
                 .build();
 
         posts.add(post);
 
-        return "redirect:/posts";
+        return "redirect:/posts"; //리다이렉트
     }
 
-    private String getFormHtml(String errorMsg, String title, String content) {
-        return """
-                <div>%s</div>
-                <form method="post">
-                  <input type="text" name="title" placeholder="제목" value="%s"/> <br>
-                  <textarea name="content">%s</textarea> <br>
-                  <input type="submit" value="등록" /> <br>
-                </form>
-                <a href="/posts">목록</a>
-                """.formatted(errorMsg, title, content);
-    }
 
     @GetMapping
-    @ResponseBody
-    private String showList() {
+    private String showList(Model model) {
+
         String lis = posts.stream()
-                .map(post -> "<li>" + post.getTitle() + "</li>")
+                .map(p -> "<li>" + p.getTitle() + "</li>")
                 .collect(Collectors.joining());
 
         String ul = "<ul>" + lis + "</ul>";
 
-        return """
-                <div>글 목록</div>
-                %s
-                <a href="/posts/write">글쓰기</a>
-                """.formatted(ul);
+        model.addAttribute("posts", posts);
+
+        return "domain/post/post/list";
     }
+
 
 }
